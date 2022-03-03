e5e94615eaa0d1a58b87f6809b985ab9297ee2de
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2022-03-03 21:20:44 +0700

    add swagger2

---
+++ src/main/java/cybersoft/javabackend/java16giragv/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package cybersoft.javabackend.java16giragv.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket getDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("cybersoft.javabackend.java16giragv"))
+				.build()
+				.apiInfo(new ApiInfoBuilder().title("Gira Application")
+						.version("1.0.0")
+						.description("This project is used for education purpose only.")
+						.contact(new Contact("Tuan Phan", "https://tuanphan.dev/", "contact@tuanphan.dev"))
+						.build());
+		
+	}
+}
